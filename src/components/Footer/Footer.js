e30618ce490732888f3c55d60cd0a071a1a1985d
راكينول إسلام <you@example.com>
2022-10-25 16:02:04 +0600

    signup start

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -50,7 +50,7 @@ const Footer = () => {
             </div>
           </div>
           <div>
-            <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            <p>Copyright © 2022 - All right reserved by E-learning Ltd</p>
           </div>
         </footer>
       </div>
