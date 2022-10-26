1815001b80f288961dcc372cd0efce6be4825808
راكينول إسلام <you@example.com>
2022-10-26 23:37:37 +0600

    all done

--- src/components/Courses/Details/Details.js
+++ src/components/Courses/Details/Details.js
@@ -3,9 +3,11 @@ import './Details.css';
 
 const Details = ({data}) => {
     return (
-      <div className='dtls text-black'>
-        <h1 className='text-2xl font-bold'>Details of: {data.title}</h1>
-        <p className='font-semibold'>{data.description}</p>
+      <div>
+        <div className="dtls text-white bg-slate-800">
+          <h1 className="text-2xl font-bold">Details of: {data.title}</h1>
+          <p className="font-semibold">{data.description}</p>
+        </div>
       </div>
     );
 };
--- src/components/Courses/SideNav.js
+++ src/components/Courses/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import Details from './Details/Details';
 
 const SideNav = () => {
@@ -8,14 +8,18 @@ const SideNav = () => {
       <div className="cart">
         <div>
           <div>
-            {
-              datas.map( data => <Details
-              key={data._id}
-              data={data}
-              ></Details>)
-            }
+            <h1 className="text-3xl font-bold mt-3 text-black">
+              ❝ COURSE DETAILS PAGE ❞
+            </h1>
+            <Link to="/courses">
+              <button className="btn btn-wide bg-slate-800 my-5 text-white">
+                Back to courses
+              </button>
+            </Link>
+            {datas.map((data) => (
+              <Details key={data._id} data={data}></Details>
+            ))}
           </div>
-          
         </div>
       </div>
     );
